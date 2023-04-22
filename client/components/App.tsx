e15dd5d57dcbff4a604c8c92bb0e409e7bdddee1
Samuel Lee <leesamuel423@gmail.com>
2023-04-22 15:30:05 -0400

    add to App.tsx to test inputLogger.ts

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 function App() {
-  return <p>Hello World</p>;
+  return (
+    <div>
+        <h1>Hello World</h1>
+        <button>Click Me</button>
+        <button>Click Me 2</button>
+    </div>
+  )
 }
 
 export default App;
