4732bbc9faa5b27a21cbd1657c23536fddb68a4d
Samuel Lee <leesamuel423@gmail.com>
2023-04-24 02:17:55 -0400

    update comments for code clarity

--- client/utils/inputLogger.ts
+++ client/utils/inputLogger.ts
@@ -10,9 +10,8 @@ function getRelativeXPath(element: HTMLElement | null): string {
   if (!element) return '';
   // Get the element's tag name and convert it to lowercase to follow xPath conventions
   const tagName = element.tagName.toLowerCase();
-  // Check if the element has the unique attribute and construct the attribute part of the XPath
+  // Array of unique attributes to use for the XPath
   const uniqueAttributes = ['data-cy', 'data-test', 'data-testid', 'id'];
- 
   let attr = '';
 
   // Iterate through the uniqueAttributes array and use the first one found on the element
@@ -22,9 +21,10 @@ function getRelativeXPath(element: HTMLElement | null): string {
       break;
     }
   }
-
+  // If a unique attribute is found, return the xPath using that attribute
   if (attr) return `//${tagName}${attr}`;
 
+  // Calculate position of the element among siblings with the same tag
   let position = 1;
   let sibling = element.previousElementSibling;
   while (sibling) {
@@ -32,6 +32,7 @@ function getRelativeXPath(element: HTMLElement | null): string {
     sibling = sibling.previousElementSibling;
   }
 
+  // Generate parent element's xPath and append current element's tag name and position
   const parentXPath = getRelativeXPath(element.parentElement);
   return `${parentXPath}/${tagName}[${position}]`;
 }
