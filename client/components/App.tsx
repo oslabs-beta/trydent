d93fa3696d44955ad71da116212e3c9baaffa72f
Samuel Lee <leesamuel423@gmail.com>
2023-04-26 04:06:11 -0400

    testing App.tsx

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -3,38 +3,12 @@ import EventLogger from './EventLogger';
 
 
 const App: React.FC = () => {
-  const [events, setEvents] = useState<any[]>(JSON.parse(localStorage.getItem('loggedEvents') || '[]'));
-
-  useEffect(() => {
-    if (typeof chrome === 'undefined' || typeof chrome.runtime === 'undefined') {
-      console.warn('chrome.runtime is not available');
-      return;
-    }
-  
-    const listener = (request, sender, sendResponse) => {
-      console.log('Received message:', request);
-      if (request.action === 'logEvent') {
-        const newEvents = [...events, request.eventData];
-        setEvents(newEvents);
-        localStorage.setItem('loggedEvents', JSON.stringify(newEvents));
-      }
-    };
-  
-    chrome.runtime.onMessage.addListener(listener);
-    return () => {
-      chrome.runtime.onMessage.removeListener(listener);
-    };
-  }, [events]);
-  
-  
 
   return (
     <div>
       <h1>Logged Events</h1>
+      <p>Pain</p>
       <EventLogger />
-      {events.map((event, index) => (
-        <pre key={index}>{JSON.stringify(event, null, 2)}</pre>
-      ))}
     </div>
   );
 };
