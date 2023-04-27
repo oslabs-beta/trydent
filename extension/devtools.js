9ecf24a47d16f48127d5d4571f2ae03e6859d190
Nicholas <lynick14@gmail.com>
2023-04-27 19:54:28 -0400

    can render event array to dom Co-authored-by: Samuel Lee <leesamuel423@users.noreply.github.com> Co-authored-by: Eric Dunn <ELDunn@users.noreply.github.com> Co-authored-by: Nicholas Ly <nicholas-l-ly@users.noreply.github.com> Co-authored-by: Jake Gillan <JakeGillan@users.noreply.github.com> Co-authored-by: Alastair <alastairsounds@users.noreply.github.com>

--- extension/devtools.js
+++ extension/devtools.js
@@ -11,29 +11,43 @@ const backgroundPageConnection = chrome.runtime.connect({
 });
 
 const eventArr = [];
+
+
 chrome.runtime.onMessage.addListener((message) => {
   // Handle responses from the background page, if any
-  console.log('message received', message);
   eventArr.push(message)
-  console.log(eventArr)
+  console.log('message received:', message);
+  console.log('all the events: ', eventArr)
 });
-const panel = document.getElementById('panel');
 
-function submitToPanel(messages){
-  console.log('submitting')
-  messages.forEach((message)=>{
-    console.log(message)
-    const test = document.createElement = 'div';
-      // made it into a string
-    test.innerText = `Action: ${message.action} \n Selector: ${message.xPath} \n URL: ${message.URL} \n Input: ${message.input}`;
-    panel.appendChild(test);
-  })
-}
+
+const panel = document.getElementById('panel');
+//  function submitToPanel(eventArr){
+//   console.log('submitting')
+//   console.log('array', eventArr)
+//   eventArr.forEach((event)=>{
+//     console.log('in panel', event)
+//     // const test = document.createElement('div');
+//     //   // made it into a string
+//     // test.innerText = `Action: ${message.action} \n Selector: ${message.xPath} \n URL: ${message.URL} \n Input: ${message.input}`;
+//     // panel.appendChild(test);
+//   })
+// }
 
 const submitButton =  document.createElement("button")
-submitButton.innerText = 'submit'
-submitButton.onclick = submitToPanel(eventArr)
 panel.appendChild(submitButton)
+submitButton.innerText = 'submit'
+submitButton.addEventListener('click', (e)=>{
+  console.log('clicked submit') 
+  console.log('events array?', eventArr)
+  eventArr.forEach((message)=>{
+    testItem = document.createElement('div');
+    testItem.innerText = `Action: ${message.action} \n Selector: ${message.selector} \n URL: ${message.URL} \n Input: ${message.input}`;
+    panel.appendChild(testItem)
+    eventArr.splice(0,eventArr.length);
+    console.log('cleared the event Arr: ', eventArr)
+  })
+})
 
 
 // Relay the tab ID to the background page as an object
