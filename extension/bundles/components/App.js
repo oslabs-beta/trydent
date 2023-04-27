89110574f39a72d7ac56df42de149ab4c3d9531b
Nicholas <lynick14@gmail.com>
2023-04-27 18:29:55 -0400

    can get array

--- extension/bundles/components/App.js
+++ extension/bundles/components/App.js
@@ -1,6 +1,7 @@
 import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
+import CodeBlock from './CodeBlock';
 import EventLogger from './EventLogger';
 const App = () => {
-    return (_jsxs("div", { children: [_jsx("h1", { children: "Logged Events" }), _jsx("p", { children: "Pain" }), _jsx(EventLogger, {})] }));
+    return (_jsxs("div", { children: [_jsx("h1", { children: "Logged Events" }), _jsx("p", { children: "Pain" }), _jsx("h3", { children: "Code Block" }), _jsx(CodeBlock, {}), _jsx(EventLogger, {})] }));
 };
 export default App;
--- extension/devtools.js
+++ extension/devtools.js
@@ -10,14 +10,31 @@ const backgroundPageConnection = chrome.runtime.connect({
   name: 'devtools-page',
 });
 
+const eventArr = [];
 chrome.runtime.onMessage.addListener((message) => {
   // Handle responses from the background page, if any
   console.log('message received', message);
-
-  const panel = document.getElementById('panel');
-  // made it into a string
-  panel.innerText = `Action: ${message.action} \n Selector: ${message.xPath} \n URL: ${message.URL} \n Input: ${message.input}`;
+  eventArr.push(message)
+  console.log(eventArr)
 });
+const panel = document.getElementById('panel');
+
+function submitToPanel(messages){
+  console.log('submitting')
+  messages.forEach((message)=>{
+    console.log(message)
+    const test = document.createElement = 'div';
+      // made it into a string
+    test.innerText = `Action: ${message.action} \n Selector: ${message.xPath} \n URL: ${message.URL} \n Input: ${message.input}`;
+    panel.appendChild(test);
+  })
+}
+
+const submitButton =  document.createElement("button")
+submitButton.innerText = 'submit'
+submitButton.onclick = submitToPanel(eventArr)
+panel.appendChild(submitButton)
+
 
 // Relay the tab ID to the background page as an object
 backgroundPageConnection.postMessage({
