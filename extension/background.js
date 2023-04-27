14294082d4439ccec49299645b4accee748521f1
Samuel Lee <leesamuel423@gmail.com>
2023-04-27 13:15:10 -0400

    update content-script logic and background logic
    Capturing correct events and sending correctly to service worker
    
    Co-authored-by: Eric Dunn <ELDunn@users.noreply.github.com>
    Co-authored-by: Nicholas Ly <nicholas-l-ly@users.noreply.github.com>
    Co-authored-by: Jake Gillan <JakeGillan@users.noreply.github.com>
    Co-authored-by: Alastair <alastairsounds@users.noreply.github.com>

--- extension/background.js
+++ extension/background.js
@@ -1,40 +1,68 @@
 // upon connection execute the content-script
-chrome.runtime.onConnect.addListener(function(devToolsConnection) {
-    // assign the listener function to a variable so we can remove it later
-    const devToolsListener = function(message, sender, sendResponse) {
-        // console.log('we are in?')
-        // console.log(message)
-        // Inject a content script into the identified tab
-        chrome.scripting.executeScript(
-            {
-                target:{tabId:message.tabId},
-                files: [message.scriptToInject]
-            });
-    }
-    // add the listener
-    devToolsConnection.onMessage.addListener(devToolsListener);
-
-    chrome.tabs.onActivated.addListener((tabInfo)=>{
-      const tabId = tabInfo.tabId;
-      console.log('changed to tab: ', tabId);
-      chrome.scripting.executeScript({
-        target:{tabId:message.tabId},
-        files: [message.scriptToInject]
-
-      })
-    })
-
-
-    chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-        // Check if the message is a click event
-        if (message.action === 'click') {
-            console.log('clicked, message: ', message)
-          return
-        }
-      });
-
-    // removes the event listener when disconnecting
-    devToolsConnection.onDisconnect.addListener(function() {
-         devToolsConnection.onMessage.removeListener(devToolsListener);
+chrome.runtime.onConnect.addListener((devToolsConnection) => {
+  // assign the listener function to a variable so we can remove it later
+  const devToolsListener = function (message, sender, sendResponse) {
+    // console.log('we are in?')
+    // console.log(message)
+    // Inject a content script into the identified tab
+    chrome.scripting.executeScript({
+      target: { tabId: message.tabId },
+      files: [message.scriptToInject],
     });
+  };
+  // add the listener
+  devToolsConnection.onMessage.addListener(devToolsListener);
+
+  chrome.tabs.onActivated.addListener((tabInfo) => {
+    const { tabId } = tabInfo;
+    console.log('changed to tab: ', tabId);
+    chrome.scripting.executeScript({
+      target: { tabId: message.tabId },
+      files: [message.scriptToInject],
+
+    });
+  });
+
+  // const contentScriptListener = (message, sender, sendResponse) => {
+  //   switch (message.action) {
+  //     case 'click':
+  //       console.log('Clicked, message: ', message);
+  //       break;
+  //     case 'focus':
+  //       console.log('Focused, message: ', message);
+  //       break;
+  //     case 'blur':
+  //       console.log('Blurred, message: ', message);
+  //       break;
+  //     case 'change':
+  //       console.log('Changed, message: ', message);
+  //       break;
+  //     default:
+  //       console.log('Unknown event, message: ', message);
+  //   }
+  // };
+  // removes the event listener when disconnecting
+  devToolsConnection.onDisconnect.addListener(() => {
+    devToolsConnection.onMessage.removeListener(devToolsListener);
+  });
+});
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  // Check if the message is a click event
+  switch (message.action) {
+    case 'click':
+      console.log('Clicked, message: ', message);
+      break;
+    case 'focus':
+      console.log('Focused, message: ', message);
+      break;
+    case 'blur':
+      console.log('Blurred, message: ', message);
+      break;
+    case 'change':
+      console.log('Changed, message: ', message);
+      break;
+    default:
+      console.log('Unknown event, message: ', message);
+  }
 });
