6152e2cdfff1ae612244be5113dac187570d4128
Samuel Lee <leesamuel423@gmail.com>
2023-04-28 00:32:58 -0400

    updated bundles

--- extension/bundles/components/App.js
+++ extension/bundles/components/App.js
@@ -1,10 +1,10 @@
 import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
 import CodeBlock from './pages/CodeBlock';
-import TopBar from './topBar';
+import TopBar from './TopBar';
 import { Routes, Route } from 'react-router-dom';
-import WelcomePage from './pages/welcomePage';
-import TestPage from './pages/testPage';
+import WelcomePage from './pages/WelcomePage';
+import TestPage from './pages/TestPage';
 const App = () => {
-    return (_jsxs("div", { className: 'router', children: [_jsx("div", { children: "This is my App.tsx" }), _jsx(TopBar, {}), _jsxs(Routes, { children: [_jsx(Route, { path: "/", element: _jsx(WelcomePage, {}) }), _jsx(Route, { path: "/testPage", element: _jsx(TestPage, {}) }), _jsx(Route, { path: "/codeBlock", element: _jsx(CodeBlock, {}) })] })] }));
+    return (_jsxs("div", { className: 'router', children: [_jsx(TopBar, {}), _jsxs(Routes, { children: [_jsx(Route, { path: "/", element: _jsx(WelcomePage, {}) }), _jsx(Route, { path: "/testPage", element: _jsx(TestPage, {}) }), _jsx(Route, { path: "/codeBlock", element: _jsx(CodeBlock, {}) })] })] }));
 };
 export default App;
