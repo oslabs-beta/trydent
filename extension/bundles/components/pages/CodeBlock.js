44f3dc548431b63cc8d5b7aa8cbd32094a7cd31f
Samuel Lee <leesamuel423@gmail.com>
2023-04-28 15:41:41 -0400

    fixed copy functionality to work in dev tools
    Co-authored-by: Eric Dunn <ELDunn@users.noreply.github.com>
    Co-authored-by: Nicholas Ly <nicholas-l-ly@users.noreply.github.com>
    Co-authored-by: Jake Gillan <JakeGillan@users.noreply.github.com>
    Co-authored-by: Alastair <alastairsounds@users.noreply.github.com>

--- extension/bundles/components/pages/CodeBlock.js
+++ extension/bundles/components/pages/CodeBlock.js
@@ -1,16 +1,38 @@
 import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
 import { useState } from 'react';
 import { sampleTextFormatted } from '../../utils/testCreator';
+/**
+ * CodeBlock component displays the generated test code and provides functionality to copy to clipboard
+ *
+ * @component
+ * @returns {ReactElement} JSX code for rendering the component
+ */
 const CodeBlock = () => {
+    // State to track if code has been copied to clipboard
     const [copied, setCopied] = useState(false);
-    const handleCopy = () => {
+    /**
+     * handleCopy function copies sampleTextFormatted to clipboard
+     * Also updates copied status and resets it after 2 seconds
+     *
+     * @async
+     * @function
+     */
+    const handleCopy = async () => {
+        // creates temporary textarea element. setting the value to sample text and appending to body
         const textArea = document.createElement('textarea');
         textArea.value = sampleTextFormatted;
         document.body.appendChild(textArea);
+        // selects the text in the text area
         textArea.select();
-        navigator.clipboard.writeText(sampleTextFormatted);
-        document.body.removeChild(textArea);
-        setCopied(true);
+        try {
+            // try to copy selected content to the clipboard
+            document.execCommand('copy');
+            setCopied(true);
+        }
+        catch (err) {
+            console.log('Unable to copy text', err);
+        }
+        // remove textarea from document body after 2 seconds and reset copied status
         setTimeout(() => {
             setCopied(false);
         }, 2000);
