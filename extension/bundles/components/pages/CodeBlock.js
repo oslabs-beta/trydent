8e6575460ed676952b81159ac7c5ef990317d3c4
JakeGillan <126023939+JakeGillan@users.noreply.github.com>
2023-04-29 15:18:19 -0500

    clean up testCreator.ts and add code imports to devtools.js

--- extension/bundles/components/pages/CodeBlock.js
+++ extension/bundles/components/pages/CodeBlock.js
@@ -1,6 +1,34 @@
 import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
 import { useState } from 'react';
-import { sampleTextFormatted } from '../../utils/testCreator';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { oneDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import prettier from 'prettier/standalone';
+import parserBabel from 'prettier/parser-babel';
+const prettierFormat = (input) => {
+    return prettier.format(input, {
+        parser: 'babel',
+        plugins: [parserBabel],
+    });
+};
+let sampleText = `//High level description on how the button should be clicked
+describe("click on the thing", () => {
+  beforeEach(() => {
+    cy.visit("localhost:3000");
+    cy.window().should("have.property", "appReady", true);
+  });
+
+  it("should be a click3", () => {
+    cy.xpath('[".XPATH"]').click();
+    cy.url().should("include", "/user/login");
+    cy.xpath('[".XPATH"]').input("typed into the box");
+  });
+  it("should be a input4", () => {
+    cy.xpath('[".XPATH"]').click();
+    cy.url().should("include", "/user/login");
+    cy.xpath('[".XPATH"]').input("typed into the box");
+  });
+});`;
+let sampleTextFormatted = prettierFormat(sampleText);
 /**
  * CodeBlock component displays the generated test code and provides functionality to copy to clipboard
  *
@@ -30,7 +58,7 @@ const CodeBlock = () => {
         textArea.select();
         try {
             // try to copy selected content to the clipboard
-            document.execCommand('copy');
+            await navigator.clipboard.writeText(sampleTextFormatted);
             setCopied(true);
         }
         catch (err) {
@@ -41,6 +69,31 @@ const CodeBlock = () => {
             setCopied(false);
         }, 2000);
     };
-    return (_jsxs("div", { className: "codeBlock", children: [_jsx("h1", { children: "Generated Test" }), _jsxs("pre", { children: [_jsx("button", { onClick: handleCopy, disabled: copied, className: copied ? 'copied' : '', children: copied ? 'Copied to clipboard!' : 'Copy' }), _jsx("code", { id: "codeBlock", children: sampleTextFormatted })] }), _jsxs("p", { children: ["Thank you for supporting TRYDENT. If you enjoyed, please give our ", _jsx("a", { href: "https://github.com/oslabs-beta/trydent", target: "_blank", children: "Github" }), " a star!"] })] }));
+    return (_jsxs("div", { className: "codeBlock", children: [_jsx("h1", { children: "Generated Test" }), _jsxs("pre", { children: [_jsx("button", { onClick: handleCopy, disabled: copied, className: copied ? 'copied' : '', children: copied ? 'Copied to clipboard!' : 'Copy' }), _jsx(SyntaxHighlighter
+                    // automatic parsing of the language set to javascript
+                    , { 
+                        // automatic parsing of the language set to javascript
+                        language: "javascript", 
+                        // use the oneDark theme
+                        style: oneDark, 
+                        // set the pre tag style
+                        customStyle: {
+                            background: '#1A1A1A',
+                        }, 
+                        // set the code tag style
+                        codeTagProps: {
+                            style: {
+                                background: '#1A1A1A',
+                            },
+                        }, 
+                        // must be enabled for lineProps to work
+                        wrapLines: true, 
+                        // set the line style of the span that wraps each span of code
+                        lineProps: {
+                            style: {
+                                // set padding-right to make padding symmetrical
+                                paddingRight: '1em',
+                            },
+                        }, children: sampleTextFormatted })] }), _jsxs("p", { children: ["Thank you for supporting TRYDENT. If you enjoyed, please give our ", _jsx("a", { href: "https://github.com/oslabs-beta/trydent", target: "_blank", children: "Github" }), " a star!"] })] }));
 };
 export default CodeBlock;
