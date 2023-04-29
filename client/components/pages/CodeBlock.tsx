ae2aead4e90e9ad91770de0c6f63c3b576a14953
JakeGillan <126023939+JakeGillan@users.noreply.github.com>
2023-04-29 15:18:09 -0500

    clean up testCreator.ts and add code imports to devtools.js

--- client/components/pages/CodeBlock.tsx
+++ client/components/pages/CodeBlock.tsx
@@ -1,9 +1,41 @@
 import React, { useState, useEffect } from 'react';
-import { sampleTextFormatted } from '../../utils/testCreator';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { oneDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import prettier from 'prettier/standalone';
+import parserBabel from 'prettier/parser-babel';
+
+const prettierFormat = (input: string): string => {
+  return prettier.format(input, {
+    parser: 'babel',
+    plugins: [parserBabel],
+  });
+};
+
+let sampleText = `//High level description on how the button should be clicked
+describe("click on the thing", () => {
+  beforeEach(() => {
+    cy.visit("localhost:3000");
+    cy.window().should("have.property", "appReady", true);
+  });
+
+  it("should be a click3", () => {
+    cy.xpath('[".XPATH"]').click();
+    cy.url().should("include", "/user/login");
+    cy.xpath('[".XPATH"]').input("typed into the box");
+  });
+  it("should be a input4", () => {
+    cy.xpath('[".XPATH"]').click();
+    cy.url().should("include", "/user/login");
+    cy.xpath('[".XPATH"]').input("typed into the box");
+  });
+});`
+let sampleTextFormatted = prettierFormat(sampleText);
+
+
 
 /**
  * CodeBlock component displays the generated test code and provides functionality to copy to clipboard
- * 
+ *
  * @component
  * @returns {ReactElement} JSX code for rendering the component
  */
@@ -14,7 +46,7 @@ const CodeBlock: React.FC = () => {
   /**
    * handleCopy function copies sampleTextFormatted to clipboard
    * Also updates copied status and resets it after 2 seconds
-   * 
+   *
    * @async
    * @function
    */
@@ -31,7 +63,7 @@ const CodeBlock: React.FC = () => {
     textArea.select();
     try {
       // try to copy selected content to the clipboard
-      document.execCommand('copy');
+      await navigator.clipboard.writeText(sampleTextFormatted);
       setCopied(true);
     } catch (err) {
       console.log('Unable to copy text', err);
@@ -55,7 +87,33 @@ const CodeBlock: React.FC = () => {
           {copied ? 'Copied to clipboard!' : 'Copy'}
         </button>
         {/* Render the code block with the sample text */}
-        <code id="codeBlock">{sampleTextFormatted}</code>
+        <SyntaxHighlighter
+          // automatic parsing of the language set to javascript
+          language="javascript"
+          // use the oneDark theme
+          style={oneDark}
+          // set the pre tag style
+          customStyle={{
+            background: '#1A1A1A',
+          }}
+          // set the code tag style
+          codeTagProps={{
+            style: {
+              background: '#1A1A1A',
+            },
+          }}
+          // must be enabled for lineProps to work
+          wrapLines={true}
+          // set the line style of the span that wraps each span of code
+          lineProps={{
+            style: {
+              // set padding-right to make padding symmetrical
+              paddingRight: '1em',
+            }, 
+          }}
+        >
+          {sampleTextFormatted}
+        </SyntaxHighlighter>
       </pre>
       <p>Thank you for supporting TRYDENT. If you enjoyed, please give our <a href="https://github.com/oslabs-beta/trydent" target="_blank">Github</a> a star!</p>
     </div>
