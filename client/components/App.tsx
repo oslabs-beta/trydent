5f4972b2763c4092ff449823b7deb259922f0122
Alastair Scheuermann <alastairscheuermann@gmail.com>
2023-05-01 09:29:40 -0600

    reordered and grouped import statements
    Co-authored-by: Samuel Lee <leesamuel423@users.noreply.github.com>
    Co-authored-by: Eric Dunn <ELDunn@users.noreply.github.com>
    Co-authored-by: Nicholas Ly <nicholas-l-ly@users.noreply.github.com>
    Co-authored-by: Jake Gillan <JakeGillan@users.noreply.github.com>
    Co-authored-by: Alastair <alastairsounds@users.noreply.github.com>

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,11 +1,13 @@
-// first commit
+// imports we use
 import React, { useState, useEffect } from 'react';
-import CodeBlock from './pages/CodeBlock'
-import EventLogger from './EventLogger';
-import TopBar from './TopBar';
 import { Routes, Route, useNavigate } from 'react-router-dom';
+import TopBar from './TopBar';
 import WelcomePage from './pages/WelcomePage';
 import TestPage from './pages/TestPage';
+import CodeBlock from './pages/CodeBlock'
+
+// imports necessary to prevent bundling errors
+import EventLogger from './EventLogger';
 
 const App: React.FC = () => {
   return (
