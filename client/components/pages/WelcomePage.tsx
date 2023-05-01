ceb7c17eb92fcbb96af6afa40d020b5c6eb7385f
Alastair Scheuermann <alastairscheuermann@gmail.com>
2023-05-01 10:38:26 -0600

    Basic cleanup - converted to TS - removed commented out code - formatted indents
    Co-authored-by: Samuel Lee <leesamuel423@users.noreply.github.com>
    Co-authored-by: Eric Dunn <ELDunn@users.noreply.github.com>
    Co-authored-by: Nicholas Ly <nicholas-l-ly@users.noreply.github.com>
    Co-authored-by: Jake Gillan <JakeGillan@users.noreply.github.com>
    Co-authored-by: Alastair <alastairsounds@users.noreply.github.com>

--- client/components/pages/WelcomePage.tsx
+++ client/components/pages/WelcomePage.tsx
@@ -1,34 +1,30 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom'; 
+import { useNavigate } from 'react-router-dom';
 
 const WelcomePage: React.FC = () => {
+  const navigate = useNavigate();
 
-    // const [testDescribe, setTestDescribe] = useState('');
-    const navigate = useNavigate();
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    navigate('/testPage');
+  };
 
-    const handleClick = (event) => {
-        event.preventDefault();
-        // console.log(event.target[0].value);
-        // setTestDescribe(event.target[0].value);
-        navigate('/testPage');
-    }
-
-    return(
-      <div className="welcomePage">
-        <h1>New Test</h1>
-        <textarea placeholder="describe statement"/>	         
-        <button onClick={handleClick}>Start Test</button>	          
-        <details>
-          <summary>Instructions</summary>
-          <ol>
-            <li>Enter your describe statement for your test!</li>
-            <li>Once you are ready, "Start Test"</li>
-          </ol>
-          <p>Remember, `describe` breaks your test suite into components. </p>
-          <p>`it` statements further break down `describe` tests into smaller individual tests</p>
-        </details>
-      </div>
-    )
-}
+  return (
+    <div className="welcomePage">
+      <h1>New Test</h1>
+      <textarea placeholder="describe statement" />
+      <button onClick={handleClick}>Start Test</button>
+      <details>
+        <summary>Instructions</summary>
+        <ol>
+          <li>Enter your describe statement for your test!</li>
+          <li>Once you are ready, "Start Test"</li>
+        </ol>
+        <p>Remember, `describe` breaks your test suite into components. </p>
+        <p>`it` statements further break down `describe` tests into smaller individual tests</p>
+      </details>
+    </div>
+  );
+};
 
 export default WelcomePage;
