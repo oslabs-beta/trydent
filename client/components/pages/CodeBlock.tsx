b05322322bae119310e2c6bb8a429609ddfde4c9
Alastair Scheuermann <alastairscheuermann@gmail.com>
2023-05-01 13:38:02 -0600

    CodeBlock.tsx basic cleanup - fully converted to TS - formatted indents / spaces
    Co-authored-by: Samuel Lee <leesamuel423@users.noreply.github.com>
    Co-authored-by: Eric Dunn <ELDunn@users.noreply.github.com>
    Co-authored-by: Nicholas Ly <nicholas-l-ly@users.noreply.github.com>
    Co-authored-by: Jake Gillan <JakeGillan@users.noreply.github.com>
    Co-authored-by: Alastair <alastairsounds@users.noreply.github.com>

--- client/components/pages/CodeBlock.tsx
+++ client/components/pages/CodeBlock.tsx
@@ -1,12 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'; // ###TODO: useEffect is never used. Remove it?
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { oneDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import prettier from 'prettier/standalone';
 import parserBabel from 'prettier/parser-babel';
-import { describeCreator } from '../../utils/testCreator';
+import GitHubButton from 'react-github-btn';
 
-import GitHubButton from 'react-github-btn'
-
+/**
+ * formats the input string using prettier
+ * @param input - string to be formatted
+ * @returns {string} - formatted string
+ */
 const prettierFormat = (input: string): string => {
   return prettier.format(input, {
     parser: 'babel',
@@ -14,46 +17,46 @@ const prettierFormat = (input: string): string => {
   });
 };
 
+// ###TODO: delete once page events are properly implemented
+// changed to be poorly formatted, proving that prettier works
 let sampleText = `//High level description on how the button should be clicked
 describe("click on the thing", () => {
-  beforeEach(() => {
+        beforeEach(() => {
     cy.visit("localhost:3000");
     cy.window().should("have.property", "appReady", true);
-  });
+});
 
   it("should be a click3", () => {
     cy.xpath('[".XPATH"]').click();
-    cy.url().should("include", "/user/login");
-    cy.xpath('[".XPATH"]').input("typed into the box");
+        cy.url().should("include", "/user/login");
+      cy.xpath('[".XPATH"]').input("typed into the box");
   });
   it("should be a input4", () => {
-    cy.xpath('[".XPATH"]').click();
-    cy.url().should("include", "/user/login");
+    cy.xpath('[".XPATH"]').click()
+    cy.url().should("include", "/user/login")
     cy.xpath('[".XPATH"]').input("typed into the box");
   });
-});`
+});`;
 let sampleTextFormatted = prettierFormat(sampleText);
 
-
-
 /**
- * CodeBlock component displays the generated test code and provides functionality to copy to clipboard
+ * Displays the generated code and provides functionality of copying to clipboard
  *
  * @component
- * @returns {ReactElement} JSX code for rendering the component
+ * @returns {ReactElement} - JSX code for rendering the component
  */
 const CodeBlock: React.FC = () => {
   // State to track if code has been copied to clipboard
   const [copied, setCopied] = useState(false);
 
   /**
-   * handleCopy function copies sampleTextFormatted to clipboard
-   * Also updates copied status and resets it after 2 seconds
+   * Copies sampleTextFormatted to clipboard
+   * Then updates 'copied' status and resets it after 2 seconds
    *
    * @async
    * @function
    */
-  const handleCopy = async () => {
+  const handleCopy = async (): Promise<void> => {
     // creates temporary textarea element. setting the value to sample text and appending to body
     const textArea = document.createElement('textarea');
     textArea.value = sampleTextFormatted;
@@ -64,13 +67,15 @@ const CodeBlock: React.FC = () => {
     document.body.appendChild(textArea);
     // selects the text in the text area
     textArea.select();
+
+    // try to copy selected content to the clipboard
     try {
-      // try to copy selected content to the clipboard
       await navigator.clipboard.writeText(sampleTextFormatted);
       setCopied(true);
     } catch (err) {
       console.log('Unable to copy text', err);
     }
+
     // remove textarea from document body after 2 seconds and reset copied status
     setTimeout(() => {
       setCopied(false);
@@ -82,44 +87,37 @@ const CodeBlock: React.FC = () => {
       <h1>Generated Test</h1>
       <pre>
         {/* Render the copy button and change its text and class depending on the 'copied' state */}
-        <button
-          onClick={handleCopy}
-          disabled={copied}
-          className={copied ? 'copied' : ''}
-        >
+        <button onClick={handleCopy} disabled={copied} className={copied ? 'copied' : ''}>
           {copied ? 'Copied to clipboard!' : 'Copy'}
         </button>
-        {/* Render the code block with the sample text */}
         <SyntaxHighlighter
-          // automatic parsing of the language set to javascript
           language="javascript"
-          // use the oneDark theme
           style={oneDark}
-          // set the pre tag style
-          customStyle={{
-            background: '#1A1A1A',
-          }}
-          // set the code tag style
-          codeTagProps={{
-            style: {
-              background: '#1A1A1A',
-            },
-          }}
+          customStyle={{ background: '#1A1A1A' }}
+          codeTagProps={{ style: { background: '#1A1A1A' } }}
           // must be enabled for lineProps to work
           wrapLines={true}
           // set the line style of the span that wraps each span of code
-          lineProps={{
-            style: {
-              // set padding-right to make padding symmetrical
-              paddingRight: '1em',
-            }, 
-          }}
+          // set padding-right of scrollable text to make padding symmetrical
+          lineProps={{ style: { paddingRight: '1em' } }}
         >
           {sampleTextFormatted}
         </SyntaxHighlighter>
       </pre>
-      <p>Thank you for supporting TRYDENT. If you enjoyed, please give our <a href="https://github.com/oslabs-beta/trydent" target="_blank">Github</a> a star!</p>
-      <GitHubButton href="https://github.com/oslabs-beta/trydent" data-icon="octicon-star" aria-label="Star oslabs-beta/trydent on GitHub">Star</GitHubButton>
+      <p>
+        Thank you for supporting TRYDENT. If you enjoyed, please give our{' '}
+        <a href="https://github.com/oslabs-beta/trydent" target="_blank">
+          Github
+        </a>{' '}
+        a star!
+      </p>
+      <GitHubButton
+        href="https://github.com/oslabs-beta/trydent"
+        data-icon="octicon-star"
+        aria-label="Star oslabs-beta/trydent on GitHub"
+      >
+        Star
+      </GitHubButton>
     </div>
   );
 };
