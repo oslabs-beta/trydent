a2481d2d3d8b9c548d7a33eba7a77a7f33c5937f
Samuel Lee <leesamuel423@gmail.com>
2023-05-03 20:36:09 -0400

    JSDoc comments on topbar
    Co-authored-by: Eric Dunn <ELDunn@users.noreply.github.com>
    Co-authored-by: Nicholas Ly <nicholas-l-ly@users.noreply.github.com>
    Co-authored-by: Jake Gillan <JakeGillan@users.noreply.github.com>
    Co-authored-by: Alastair <alastairsounds@users.noreply.github.com>

--- client/components/TopBar.tsx
+++ client/components/TopBar.tsx
@@ -2,14 +2,22 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
 /**
- * Displays the logo and button to create new Test
+ * TopBar component that displays the logo and a button to create a new test.
+ * When the button is clicked, the user is navigated to the root path.
  *
  * @component
- * @returns {ReactElement}
+ * @returns {ReactElement} The TopBar component
  */
 const TopBar: React.FC = () => {
+  // Hook to navigate to different routes
   const navigate = useNavigate();
 
+  /**
+   * Handles the click event on the "New Test" button.
+   * Navigates to the root path when the button is clicked.
+   *
+   * @param {React.MouseEvent<HTMLButtonElement>} event - The click event
+   */
   const handleSubmit = (event: React.MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
     navigate('/');
