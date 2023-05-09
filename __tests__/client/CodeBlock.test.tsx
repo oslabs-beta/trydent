cabdc1b6f55a16449884a783c75d2e536a338c60
Alastair Scheuermann <alastairscheuermann@gmail.com>
2023-05-08 19:00:36 -0600

    multiple describe statements
    Co-authored-by: Samuel Lee <leesamuel423@users.noreply.github.com>
    Co-authored-by: Eric Dunn <ELDunn@users.noreply.github.com>
    Co-authored-by: Nicholas Ly <nicholas-l-ly@users.noreply.github.com>
    Co-authored-by: Jake Gillan <JakeGillan@users.noreply.github.com>
    Co-authored-by: Alastair <alastairsounds@users.noreply.github.com>

--- __tests__/client/CodeBlock.test.tsx
+++ __tests__/client/CodeBlock.test.tsx
@@ -10,15 +10,21 @@ import GitHubButton from 'react-github-btn';
 describe('CodeBlock React Function Component', () => {
   it('should render the CodeBlock component', () => {});
 
+  it('should redirect the user to the GitHub repository when the "Star" button is clicked', () => {});
+});
+
+describe('CodeBlock prettierFormat function', () => {
+  it('should format code that is sent to it', () => {});
+});
+
+describe('CodeBlock handleCopy function', () => {
   it('should provide an enabled and clickable copy button', () => {});
 
   it('should display "Copied to Clipboard!" after clicking the "Copy" button', () => {});
 
   it('should copy the text to the clipboard of the user', () => {});
+});
 
-  it('should format code that is sent to it', () => {});
-
+describe('CodeBlock SyntaxHighlighter element', () => {
   it('should display the formatted code', () => {});
-
-  
 });
