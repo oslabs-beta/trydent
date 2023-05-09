39b095d858da48e8ef0db4707b8ea5452e0459fa
Alastair Scheuermann <alastairscheuermann@gmail.com>
2023-05-08 18:47:53 -0600

    first commit
    Co-authored-by: Samuel Lee <leesamuel423@users.noreply.github.com>
    Co-authored-by: Eric Dunn <ELDunn@users.noreply.github.com>
    Co-authored-by: Nicholas Ly <nicholas-l-ly@users.noreply.github.com>
    Co-authored-by: Jake Gillan <JakeGillan@users.noreply.github.com>
    Co-authored-by: Alastair <alastairsounds@users.noreply.github.com>

---
+++ __tests__/client/CodeBlock.test.tsx
@@ -0,0 +1,24 @@
+import CodeBlock from '../../client/components/pages/CodeBlock';
+
+import React, { useState, useEffect } from 'react'; // ###TODO: useEffect is never used. Remove it?
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { oneDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import prettier from 'prettier/standalone';
+import parserBabel from 'prettier/parser-babel';
+import GitHubButton from 'react-github-btn';
+
+describe('CodeBlock React Function Component', () => {
+  it('should render the CodeBlock component', () => {});
+
+  it('should provide an enabled and clickable copy button', () => {});
+
+  it('should display "Copied to Clipboard!" after clicking the "Copy" button', () => {});
+
+  it('should copy the text to the clipboard of the user', () => {});
+
+  it('should format code that is sent to it', () => {});
+
+  it('should display the formatted code', () => {});
+
+  
+});
