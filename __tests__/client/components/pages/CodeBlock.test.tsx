0005facd76f79f7e0fcc36432975fe408bfd7f6d
Alastair Scheuermann <alastairscheuermann@gmail.com>
2023-05-15 10:17:03 -0600

    Added jest.config.js and script to root folder
    Co-authored-by: Samuel Lee <leesamuel423@users.noreply.github.com>
    Co-authored-by: Eric Dunn <ELDunn@users.noreply.github.com>
    Co-authored-by: Nicholas Ly <nicholas-l-ly@users.noreply.github.com>
    Co-authored-by: Jake Gillan <JakeGillan@users.noreply.github.com>
    Co-authored-by: Alastair <alastairsounds@users.noreply.github.com>

--- __tests__/client/components/pages/CodeBlock.test.tsx
+++ __tests__/client/components/pages/CodeBlock.test.tsx
@@ -1,36 +1,90 @@
-import CodeBlock from '../../../../client/components/pages/CodeBlock';
-
-import React, { useState, useEffect } from 'react'; // ###TODO: useEffect is never used. Remove it?
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { oneDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
-import prettier from 'prettier/standalone';
-import parserBabel from 'prettier/parser-babel';
-import GitHubButton from 'react-github-btn';
-
-describe('CodeBlock React Function Component', () => {
-  it('should render the CodeBlock component', () => {});
-
-  it('should redirect the user to the GitHub repository when the "Star" button is clicked', () => {});
+describe('', () => {
+  it('', () => {});
 });
 
-describe('CodeBlock prettierFormat function', () => {
-  it('should format code that is sent to it', () => {});
-});
+// import { render, screen } from '@testing-library/react';
+// import '@testing-library/jest-dom/extend-expect';
 
-describe('CodeBlock handleCopy function', () => {
-  it('should provide an enabled and clickable copy button', () => {});
+// import CodeBlock from '../../../../client/components/pages/CodeBlock';
 
-  it('should display "Copied to Clipboard!" after clicking the "Copy" button', () => {});
+// import React, { useState, useEffect } from 'react'; // ###TODO: useEffect is never used. Remove it?
+// import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+// import { oneDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+// import prettier from 'prettier/standalone';
+// import parserBabel from 'prettier/parser-babel';
+// import GitHubButton from 'react-github-btn';
 
-  it('should copy the text to the clipboard of the user', () => {});
-});
+// describe('CodeBlock React Function Component', () => {
+//   it('should render the CodeBlock component', () => {
+//     render(<CodeBlock />);
 
-describe('CodeBlock correct output for multiple test cases', () => {
-  it('test1');
-  it('test2');
-  it('test3');
-});
+//     expect(screen.getByRole('heading')).toHaveTextContent('Generated Test');
+//     expect(screen.getByRole('button')).toHaveTextContent('Copy');
+//     expect(screen.getByRole('link')).toHaveTextContent('Github');
+//   });
 
-describe('CodeBlock SyntaxHighlighter element', () => {
-  it('should display the formatted code', () => {});
-});
+//   it('should redirect the user to the GitHub repository when the "Star" button is clicked', () => {});
+// });
+
+// describe('CodeBlock prettierFormat function', () => {
+//   it('should format code that is sent to it', () => {});
+// });
+
+// describe('CodeBlock handleCopy function', () => {
+//   it('should provide an enabled and clickable copy button', () => {});
+
+//   it('should display "Copied to Clipboard!" after clicking the "Copy" button', () => {});
+
+//   it('should copy the text to the clipboard of the user', () => {});
+// });
+
+// describe('CodeBlock correct output for multiple test cases', () => {
+//   it('test1');
+//   it('test2');
+//   it('test3');
+// });
+
+// describe('CodeBlock SyntaxHighlighter element', () => {
+//   it('should display the formatted code', () => {});
+// });
+
+// /*
+
+// <div className="codeBlock">
+//   <div className="head">
+//     <h1>Generated Test</h1>
+//     <div className="copy-button">
+//       <button onClick={handleCopy} disabled={copied} className={copied ? 'copied' : ''}>
+//         {copied ? 'Copied to clipboard!' : 'Copy'}
+//       </button>
+//     </div>
+//   </div>
+//   <div className="code">
+//     <SyntaxHighlighter
+//       language="javascript"
+//       style={oneDark}
+//       customStyle={{ background: '#1A1A1A' }}
+//       codeTagProps={{ style: { background: '#1A1A1A' } }}
+//       wrapLines={true}
+//       lineProps={{ style: { paddingRight: '1em' } }}
+//     >
+//       {formattedText ? formattedText : ''}
+//     </SyntaxHighlighter>
+//   </div>
+//   <p>
+//     Thank you for supporting TRYDENT. If you enjoyed, please give our{' '}
+//     <a href="https://github.com/oslabs-beta/trydent" target="_blank">
+//       Github
+//     </a>{' '}
+//     a star!
+//   </p>
+//   <GitHubButton
+//     href="https://github.com/oslabs-beta/trydent"
+//     data-icon="octicon-star"
+//     aria-label="Star oslabs-beta/trydent on GitHub"
+//   >
+//     Star
+//   </GitHubButton>
+// </div>;
+
+// */
---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  // ... other Jest configuration options ...
+  testMatch: [
+    '**/__tests__/**/*.test.tsx',
+    // '**/__tests__/**/*.test.js'
+  ],
+};
