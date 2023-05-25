b379d2fe29f95d0bb807688592b681baf3a3dee0
Samuel Lee <leesamuel423@gmail.com>
2023-05-25 12:31:21 -0400

    update build

--- extension/bundles/main.js
+++ extension/bundles/main.js
@@ -2,6 +2,5 @@ import { jsx as _jsx } from "react/jsx-runtime";
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import './scss/application.scss';
 import { BrowserRouter } from 'react-router-dom';
 ReactDOM.render(_jsx(React.StrictMode, { children: _jsx(BrowserRouter, { children: _jsx(App, {}) }) }), document.getElementById('root'));
